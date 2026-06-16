ae0c793ab447b3a27510e3cb7ca0b44c3935fd9a
AdityaRusmana <adityarusmana@mufid.or.id>
2026-06-16 17:34:35 +0800

    feat(types): add UnitTrait enum, trait config, expansion types

--- src/types/index.ts
+++ src/types/index.ts
@@ -58,22 +58,78 @@ export interface LevelData {
 }
 
 export type Direction = 'up' | 'down' | 'left' | 'right'
-export type DamageType = 'kinetic' | 'thermal'
+export type DamageType = 'kinetic' | 'thermal' | 'true'
+
+export enum UnitTrait {
+  BlocksTwo = 'blocks_two',
+  BlocksThree = 'blocks_three',
+  DPOnKill = 'dp_on_kill',
+  FullRefundRetreat = 'full_refund_retreat',
+  RangedAttack80 = 'ranged_attack_80',
+  AoESplash = 'aoe_splash',
+  ArtsDamage = 'arts_damage',
+  FastAttack = 'fast_attack',
+  DoubleHit = 'double_hit',
+  HealOnAttack = 'heal_on_attack',
+  HealPerHitCapped = 'heal_per_hit_capped',
+  CannotBeHealed = 'cannot_be_healed',
+  SlowOnHit = 'slow_on_hit',
+  ChainJump = 'chain_jump',
+  LinearAoE = 'linear_aoe',
+  TargetingLowestDef = 'targeting_lowest_def',
+  RangedWhenNotBlocking = 'ranged_when_not_blocking',
+  RangedAoEWhenNotBlocking = 'ranged_aoe_when_not_blocking',
+  ConditionalDamage120 = 'conditional_damage_120',
+  TakesTrueDamage = 'takes_true_damage',
+  HealAlly = 'heal_ally',
+  AttackHealsAlly = 'attack_heals_ally',
+  AoEHoT = 'aoe_hot',
+  LongRangeAttack = 'long_range_attack',
+  PassiveDPRegen = 'passive_dp_regen',
+}
+
+export interface UnitTraitConfig {
+  traitId: UnitTrait
+  value?: number
+  duration?: number
+  radius?: number
+  maxTargets?: number
+  damageFalloff?: number
+}
+
+export interface StatusEffect {
+  type: 'slow'
+  remainingDuration: number
+  factor: number
+}
+
+export interface SplashConfig {
+  radius: number
+  damageMultiplier: number
+  damageType?: DamageType
+}
 
 export interface UnitConfig {
   id: string
   name: string
+  archetype: string
+  subtypeLabel: string
   type: 'ground' | 'ranged'
   hp: number
   atk: number
   def: number
+  insulation: number
   damageType: DamageType
   attackInterval: number
   rangePattern: number[][]
+  altRangePattern?: number[][]
   blockCount: number
   dpCost: number
   redeployTime: number
   color: number
+  traits: UnitTraitConfig[]
+  splashConfig?: SplashConfig
+  canBeHealed?: boolean
 }
 
 export interface EnemyConfig {

6dc0b86d7f3ebd74f4cd534f75375e043e05353f
AdityaRusmana <adityarusmana@mufid.or.id>
2026-07-04 11:44:39 +0800

    feat(units): wire fighter icon into in-game, deployment bar, and picker

--- src/entities/Unit.ts
+++ src/entities/Unit.ts
@@ -2,7 +2,7 @@ import Phaser from 'phaser'
 import { Direction, UnitConfig, DeployedUnit } from '../types/index'
 import { Grid, TILE_SIZE } from './Grid'
 import { FONT_SIZE } from '../ui/Constants'
-import { drawCoreCasterIcon, drawSplashCasterIcon, drawBlastCasterIcon, drawChainCasterIcon, drawMechAccordCasterIcon, drawProtectorIcon, drawGuardianIcon, drawJuggernautIcon, drawFortressIcon, drawArtsProtectorIcon, drawSentryProtectorIcon, drawPioneerIcon, drawChargerIcon, drawCenturionGuardIcon, drawLordGuardIcon, drawArtsFighterIcon, drawInstructorGuardIcon } from '../ui/Components'
+import { drawCoreCasterIcon, drawSplashCasterIcon, drawBlastCasterIcon, drawChainCasterIcon, drawMechAccordCasterIcon, drawProtectorIcon, drawGuardianIcon, drawJuggernautIcon, drawFortressIcon, drawArtsProtectorIcon, drawSentryProtectorIcon, drawPioneerIcon, drawChargerIcon, drawCenturionGuardIcon, drawLordGuardIcon, drawArtsFighterIcon, drawInstructorGuardIcon, drawFighterIcon } from '../ui/Components'
 
 export class UnitSprite {
   private scene: Phaser.Scene
@@ -137,6 +137,10 @@ export class UnitSprite {
     }
     if (config.id === 'instructor_guard') {
       drawInstructorGuardIcon(this.body, 0, 0, size, config.color)
+      return
+    }
+    if (config.id === 'fighter') {
+      drawFighterIcon(this.body, 0, 0, size, config.color)
       return
     }
     if (config.id === 'pioneer') {

11947c60f0a4aeb3cd1400a61a27bc70112ac236
AdityaRusmana <adityarusmana@mufid.or.id>
2026-07-04 11:05:15 +0800

    feat(units): wire arts_fighter icon into in-game, deployment bar, and picker

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -10,7 +10,7 @@ import { CombatSystem } from '../systems/CombatSystem'
 import { HealingSystem } from '../systems/HealingSystem'
 import { UNIT_CONFIGS } from '../config/units'
 import { COLORS, FONT_SIZE, SIDEBAR_W as PANEL_W } from '../ui/Constants'
-import { makeNodeButton, drawCoreCasterIcon, drawSplashCasterIcon, drawBlastCasterIcon, drawChainCasterIcon, drawMechAccordCasterIcon, drawProtectorIcon, drawGuardianIcon, drawJuggernautIcon, drawFortressIcon, drawArtsProtectorIcon, drawSentryProtectorIcon, drawPioneerIcon, drawChargerIcon, drawCenturionGuardIcon, drawLordGuardIcon } from '../ui/Components'
+import { makeNodeButton, drawCoreCasterIcon, drawSplashCasterIcon, drawBlastCasterIcon, drawChainCasterIcon, drawMechAccordCasterIcon, drawProtectorIcon, drawGuardianIcon, drawJuggernautIcon, drawFortressIcon, drawArtsProtectorIcon, drawSentryProtectorIcon, drawPioneerIcon, drawChargerIcon, drawCenturionGuardIcon, drawLordGuardIcon, drawArtsFighterIcon } from '../ui/Components'
 import { spawnProjectile, playSwing, showWindUp, flashDamage, spawnChainBolt, spawnSplashRing, spawnExpandRing, spawnBurstParticles, spawnBuffParticles, spawnSparkHit, spawnHealCross } from '../effects/CombatEffects'
 import { SkillSystem } from '../systems/SkillSystem'
 import { saveCompletion } from '../shared/SaveData'
@@ -1472,6 +1472,8 @@ export class GameScene extends Phaser.Scene {
       drawCenturionGuardIcon(icon, iconX, iconY, iconSize, unit.color)
     } else if (unit.id === 'lord_guard') {
       drawLordGuardIcon(icon, iconX, iconY, iconSize, unit.color)
+    } else if (unit.id === 'arts_fighter') {
+      drawArtsFighterIcon(icon, iconX, iconY, iconSize, unit.color)
     } else if (unit.id === 'pioneer') {
       drawPioneerIcon(icon, iconX, iconY, iconSize, unit.color)
     } else if (unit.id === 'charger') {

182438606e08eccba4c5c537686f37d870ad6a32
AdityaRusmana <adityarusmana@mufid.or.id>
2026-07-06 20:26:45 +0800

    feat(units): wire crusher icon into in-game, deployment bar, and picker

--- src/entities/Unit.ts
+++ src/entities/Unit.ts
@@ -2,7 +2,7 @@ import Phaser from 'phaser'
 import { Direction, UnitConfig, DeployedUnit } from '../types/index'
 import { Grid, TILE_SIZE } from './Grid'
 import { FONT_SIZE } from '../ui/Constants'
-import { drawCoreCasterIcon, drawSplashCasterIcon, drawBlastCasterIcon, drawChainCasterIcon, drawMechAccordCasterIcon, drawProtectorIcon, drawGuardianIcon, drawJuggernautIcon, drawFortressIcon, drawArtsProtectorIcon, drawSentryProtectorIcon, drawPioneerIcon, drawChargerIcon, drawCenturionGuardIcon, drawLordGuardIcon, drawArtsFighterIcon, drawInstructorGuardIcon, drawFighterIcon, drawSwordmasterIcon, drawSolobladeIcon, drawReaperIcon } from '../ui/Components'
+import { drawCoreCasterIcon, drawSplashCasterIcon, drawBlastCasterIcon, drawChainCasterIcon, drawMechAccordCasterIcon, drawProtectorIcon, drawGuardianIcon, drawJuggernautIcon, drawFortressIcon, drawArtsProtectorIcon, drawSentryProtectorIcon, drawPioneerIcon, drawChargerIcon, drawCenturionGuardIcon, drawLordGuardIcon, drawArtsFighterIcon, drawInstructorGuardIcon, drawFighterIcon, drawSwordmasterIcon, drawSolobladeIcon, drawReaperIcon, drawCrusherIcon } from '../ui/Components'
 
 export class UnitSprite {
   private scene: Phaser.Scene
@@ -153,6 +153,10 @@ export class UnitSprite {
     }
     if (config.id === 'reaper') {
       drawReaperIcon(this.body, 0, 0, size, config.color)
+      return
+    }
+    if (config.id === 'crusher') {
+      drawCrusherIcon(this.body, 0, 0, size, config.color)
       return
     }
     if (config.id === 'pioneer') {
